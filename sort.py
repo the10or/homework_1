f16bf901198ead57198995e3e3ef9ce8d6f37061
the10or <67517018+the10or@users.noreply.github.com>
2023-08-12 12:42:19 +0300

    initial commit

---
+++ sort.py
@@ -0,0 +1,29 @@
+import shutil
+import sys
+from pathlib import Path
+from normalize import normalize
+from scan import scan, categories
+
+
+def main(path):
+    scan(path)
+    for category, files in categories.items():
+        category_dir = path / category
+        category_dir.mkdir(exist_ok=True)
+
+        for file in files:
+            new_path = normalize(file.name)
+            file.replace(path / category / new_path)
+
+    scan(path)
+
+    arch_path = Path(path / 'archives')
+    for arch in arch_path.iterdir():
+        shutil.unpack_archive(arch, arch_path / arch.stem)
+        arch.unlink()
+
+        scan(arch_path)
+
+
+if __name__ == '__main__':
+    main(Path(sys.argv[1]))
